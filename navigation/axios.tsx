ee5f6893625438506e4fad441354db8ef28ab053
Masashi Kodaka <m_kodaka@suicaappdev.com>
2025-09-18 12:33:59 +0900

    ディレクトリの再編成

--- navigation/axios.tsx
+++
@@ -1,8 +0,0 @@
-// import axios from 'axios';
-
-// const apiClient = axios.create({
-//   baseURL: 'https://your-alb-url.amazonaws.com', // ALBのURL
-//   timeout: 5000, // タイムアウト設定（ミリ秒）
-// });
-
-// export default apiClient;
